bdaf68991c717140c7a3c83c44421eb86f2c40f7
Devin McCabe <devin.mccabe@gmail.com>
2018-03-22 15:59:00 -0400

    /

--- init.R
+++ init.R
@@ -1,20 +1,22 @@
 install.packages("devtools")
 
-pkgs <- as.data.frame(read.dcf("app/packrat/packrat.lock")[-1, , drop = FALSE])
+pkgs <- as.data.frame(read.dcf("/app/packrat/packrat.lock")[-1, , drop = FALSE])
 
 for (i in 1:nrow(pkgs)) {
   pkg <- pkgs[i, ]
+
+  message("Trying to install ", pkg$Package, " from Packrat src")
 
   if (pkg$Package %in% rownames(installed.packages())) {
     message(pkg$Package, " is already installed")
   } else if (pkg$Source == "CRAN") {
     devtools::install_local(
-      file.path("app/packrat/src", pkg$Package, paste0(pkg$Package, "_", pkg$Version, ".tar.gz")),
+      file.path("/app/packrat/src", pkg$Package, paste0(pkg$Package, "_", pkg$Version, ".tar.gz")),
       INSTALL_opts = "--no-docs --no-help --no-demo"
     )
   } else if (pkg$Source == "github") {
     devtools::install_local(
-      file.path("app/packrat/src", pkg$Package, paste0(pkg$GithubSha1, ".tar.gz")),
+      file.path("/app/packrat/src", pkg$Package, paste0(pkg$GithubSha1, ".tar.gz")),
       INSTALL_opts = "--no-docs --no-help --no-demo"
     )
   }
