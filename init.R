041b6c4ce3591f84d748e58e6b622aebc072a001
Devin McCabe <devin.mccabe@gmail.com>
2018-03-23 14:47:50 -0400

    .

--- init.R
+++ init.R
@@ -1,32 +1,32 @@
 dir.create("/app/cache")
 
-Sys.setenv(CCACHE_DIR = "/app/cache", CCACHE_BASEDIR = "/app", CCACHE_VERBOSE = "true")
+Sys.setenv(CCACHE_DIR = "/app/cache", CCACHE_BASEDIR = "/app", CCACHE_VERBOSE = "true", CCACHE_LOGFILE = "/app/ccache.log")
 
 # print(getwd())
 # list.files(getwd(), all.files = T, full.names = T, include.dirs = T)
 # list.files("/usr/bin", all.files = T, full.names = T, include.dirs = T)
 
 install.packages("utf8")
-stop(0)
-
-pkgs <- as.data.frame(read.dcf("/app/packrat/packrat.lock")[-1, , drop = FALSE])
-
-for (i in 1:nrow(pkgs)) {
-  pkg <- pkgs[i, ]
-
-  message("Trying to install ", pkg$Package)
-
-  if (pkg$Package %in% rownames(installed.packages())) {
-    message(pkg$Package, " is already installed")
-  } else if (pkg$Source == "CRAN") {
-    f <- file.path("/app/packrat/src", pkg$Package, paste0(pkg$Package, "_", pkg$Version, ".tar.gz"))
-    message("...from ", f)
-
-    remotes::install_local(f, INSTALL_opts = "--no-docs --no-help --no-demo")
-  } else if (pkg$Source == "github") {
-    f <- file.path("/app/packrat/src", pkg$Package, paste0(pkg$GithubSha1, ".tar.gz"))
-    message("...from ", f)
-
-    remotes::install_local(f, INSTALL_opts = "--no-docs --no-help --no-demo")
-  }
-}
+# stop()
+#
+# pkgs <- as.data.frame(read.dcf("/app/packrat/packrat.lock")[-1, , drop = FALSE])
+#
+# for (i in 1:nrow(pkgs)) {
+#   pkg <- pkgs[i, ]
+#
+#   message("Trying to install ", pkg$Package)
+#
+#   if (pkg$Package %in% rownames(installed.packages())) {
+#     message(pkg$Package, " is already installed")
+#   } else if (pkg$Source == "CRAN") {
+#     f <- file.path("/app/packrat/src", pkg$Package, paste0(pkg$Package, "_", pkg$Version, ".tar.gz"))
+#     message("...from ", f)
+#
+#     remotes::install_local(f, INSTALL_opts = "--no-docs --no-help --no-demo")
+#   } else if (pkg$Source == "github") {
+#     f <- file.path("/app/packrat/src", pkg$Package, paste0(pkg$GithubSha1, ".tar.gz"))
+#     message("...from ", f)
+#
+#     remotes::install_local(f, INSTALL_opts = "--no-docs --no-help --no-demo")
+#   }
+# }
